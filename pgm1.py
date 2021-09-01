ac8dcc7584fdb81630f1df3a96b279b68f6c7677
pcfsanty <pcfsanty@gmail.com>
2021-09-01 20:31:07 +0530

    third commit

--- pgm1.py
+++ pgm1.py
@@ -8,7 +8,11 @@
 # lastname = input("Enter your Last Name")
 # name = firstname+lastname
 # print("Your Full name is  " +name)
+# firstnumber=int(input("Enter num1"))
+# lastnumber=int(input("Enter num2"))
+# sum=(firstnumber+lastnumber)
+# print(sum)
 firstnumber=int(input("Enter num1"))
 lastnumber=int(input("Enter num2"))
-sum=(firstnumber+lastnumber)
-print(sum)
+diff=(firstnumber-lastnumber)
+print(diff)
