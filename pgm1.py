01bed478b289597726f4dcbd2e4238c85b03e051
pcfsanty <pcfsanty@gmail.com>
2021-09-01 20:25:18 +0530

    Second Commit

--- pgm1.py
+++ pgm1.py
@@ -1,10 +1,14 @@
-print("Gautham Kaarthik")
-print("Nithya Lakshmi N")
-a=10
-b=20
-c=a+b
-print (c)
-firstname = input("Enter your First Name")
-lastname = input("Enter your Last Name")
-name = firstname+lastname
-print("Your Full name is  " +name)
+# print("Gautham Kaarthik")
+# print("Nithya Lakshmi N")
+# a=10
+# b=20
+# c=a+b
+# print (c)
+# firstname = input("Enter your First Name")
+# lastname = input("Enter your Last Name")
+# name = firstname+lastname
+# print("Your Full name is  " +name)
+firstnumber=int(input("Enter num1"))
+lastnumber=int(input("Enter num2"))
+sum=(firstnumber+lastnumber)
+print(sum)
