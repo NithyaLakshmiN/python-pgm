99d5fbe8f72ae9ca24405144b5eb5a08394c87a5
pcfsanty <pcfsanty@gmail.com>
2021-09-01 19:58:36 +0530

    First Commit

---
+++ pgm1.py
@@ -0,0 +1,10 @@
+print("Gautham Kaarthik")
+print("Nithya Lakshmi N")
+a=10
+b=20
+c=a+b
+print (c)
+firstname = input("Enter your First Name")
+lastname = input("Enter your Last Name")
+name = firstname+lastname
+print("Your Full name is  " +name)
