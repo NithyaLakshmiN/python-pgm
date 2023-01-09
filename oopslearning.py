8d015da725f6222c456188f50239b75a8622a3bd
nithyalakshmi.n@gmail.com <nithyalakshmi.n@gmail.com>
2023-01-09 14:06:36 +0530

    This is done on 9th Jan

--- oopslearning.py
+++ oopslearning.py
@@ -7,8 +7,16 @@ class Computer:
     def config(self):
         print("config is", self.cpu, self.ram)
 
+    def nithya(self):
+        print("I am in pass",self.cpu,self.ram)
+
 
 com1 = Computer('ryan', 16)
 com2 = Computer('lolan', 20)
+com3 =Computer('lolan2323', 2023213)
+
 com1.config()
 com2.config()
+com3.nithya()
+
+
